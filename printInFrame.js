2e04d0575beb07e638945ad4cf239ee0b7b2628a
harikrish-24 <haripriya.kk2501@gmail.com>
2025-06-22 16:02:50 -0400

    Fix the bugs in printInFrame

--- printInFrame.js
+++ printInFrame.js
@@ -1,7 +1,7 @@
 const printInFrame = function(list) {
   list = list.split(' ');
   const longest = longestStr(list).length;
-  const border = repeat('*', longest);
+  const border = repeat('*', longest + 4);
 
   console.log(border);
   for (const word of list) {
@@ -11,7 +11,7 @@ const printInFrame = function(list) {
 };
 
 const repeat = function(str, times) {
-  let result = str;
+  let result = '';
 
   for (let i = 0; i < times; i++) {
     result += str;
@@ -24,6 +24,9 @@ const longestStr = function(list) {
   let longest = list[0];
 
   for (const str of list) {
+    if (str.length > longest.length) {
+
+    }
     longest = str;
   }
 
